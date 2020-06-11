aa884f0be273a776feb81b71932aefdec43ca44d
Obaid <obaidulhaq20@gmail.com>
2020-06-11 18:44:00 +0500

    added state in Addtransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const AddTransaction = () => {
+    const [ text, setText ] = useState('');
+    const [ amount, setAmount ] = useState(0)
   return (
     <Fragment>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(event) => setText(event.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(event) => setAmount(event.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
